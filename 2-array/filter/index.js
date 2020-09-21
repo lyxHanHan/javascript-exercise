ba2843efa2e38a5d09f2b94560b8e022e4b0be48
liyixuan <13069579956@163.com>
2020-09-21 19:47:41 +0800

    filter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,19 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const newcollection = collection.filter(ele => ele % 3 === 0);
+  console.log(newcollection);
+  return newcollection;
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  // const newCollection = collection.filter((collection,index) =>{
+  //   return collection.indexOf(collection) === index;
+  //   console.log(newCollection);
+  // })
+  const newcollection = [...new Set(collection)];
+  console.log(newcollection);
+  return newcollection;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
