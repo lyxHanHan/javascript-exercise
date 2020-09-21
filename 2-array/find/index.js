7dfb49e9ef5f5111bba48d6461fca937c4b7dde2
liyixuan <13069579956@163.com>
2020-09-21 21:04:53 +0800

    find and map

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,6 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const newcollect = collection.find(item => item.age > 10 && item.age < 20);
+  // console.log(newcollect);
+  return newcollect.name;
 }
--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,7 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  const newcollection = collection.map(item => {
+    return item.fromCharCode(32 + parseInt);
+  });
+  return newcollection;
 }
