a94bbc843931d0e3cb1cee3e3fe425632eb17dec
liyixuan <13069579956@163.com>
2020-09-21 22:27:42 +0800

    promise

--- 5-async/2-promise/index.js
+++ 5-async/2-promise/index.js
@@ -3,17 +3,27 @@ function fetchData(url) {
     const xhr = new XMLHttpRequest();
     // <-- start
     // TODO 22: 通过Promise实现异步请求
-
+    xhr.open('Get', url, false);
+    xhr.onreadystatechange = function respon() {
+      if (xhr.readyState !== 4) {
+        return;
+      }
+      if (xhr.status === 200) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    xhr.send();
     // end -->
   });
 }
 
 const URL = 'http://localhost:3000/api';
-fetchData(URL)
-  .then(result => {
-    document.writeln(JSON.parse(result).name);
+fetch(URL)
+  .then(data => {
+    console.log(data);
   })
-  .catch(error => {
-    console.error(error);
+  .catch(errorText => {
+    console.log(errorText);
   });
-console.log(1);
