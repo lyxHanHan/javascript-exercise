2d283b72166e1bfc5b0c0b6feb1d1da69a30a9fd
liyixuan <13069579956@163.com>
2020-09-22 12:52:45 +0800

    modify

--- 5-async/3-fetch/index.js
+++ 5-async/3-fetch/index.js
@@ -1,29 +1,34 @@
-// function fetchData(url) {
-//   // <-- start
-//   // TODO 23: 通过Fetch API实现异步请求
-//   if
-//   // end -->
-// }
-
-// const URL = 'http://localhost:3000/api';
-// fetchData(URL)
-//   .then(result => {
-//     document.writeln(result.name);
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
-const URL = 'http://localhost:3000/api';
-fetch(URL)
-  .then(response => {
-    if (response.status === 200) {
+function fetchData(url) {
+  // <-- start
+  // TODO 23: 通过Fetch API实现异步请求
+  return fetch(url).then(response => {
+    if (response.status >= 200 && response.status <= 299) {
       return response.json();
     }
-    Promise.reject('error msg');
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => {
-    console.log(err);
+    return Promise.reject;
   });
+  // end -->
+}
+
+const URL = 'http://localhost:3000/api';
+fetchData(URL)
+  .then(result => {
+    document.writeln(result.name);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+// const URL = 'http://localhost:3000/api';
+// fetch(URL)
+//   .then(response => {
+//     if (response.status === 200) {
+//       return response.json();
+//     }
+//     Promise.reject('error msg');
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
