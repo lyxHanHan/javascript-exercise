e7e015e37d9184ae72e6da8c47bf4ab5013ebd3a
liyixuan <13069579956@163.com>
2020-09-22 12:43:55 +0800

    4-class-5

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,15 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = value;
+    }
+    introduce() {
+        if( this.klass === undefined) {
+        return `${super.introduce()} I am a Teacher. I teacher No Class`;
+        }
+        return `${super.introduce()} I am a Teacher. I teacher ${this.klass}`;
+    }
+}
